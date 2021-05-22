089d527267d61626a7c82c685bb2929379bde059
lumines-ehzo <rattapoom.rue@gmail.com>
2021-05-23 03:30:06 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,17 @@ import React from 'react';
 import '../css/layout.css';
 
 class home extends React.Component {
-  render(){
-    return(
-        // <SwaggerUI url="" />
-        <center>
-        <div>รัฐภูมิ เรืองกล้า</div>
-        <div>6104062630212</div>
-        </center>
+  render() {
+    return (
+      // <SwaggerUI url="" />
+      <center>
+        <p><a></a></p>
+        <p><a>!!WELCOME TO MY WEB!!</a></p>
+        <p><a><div>รัฐภูมิ เรืองกล้า</div></a></p>
+        <p><a><div>6104062630212</div></a></p>
+      </center>
     );
-}
+  }
 }
 
 export default home;
